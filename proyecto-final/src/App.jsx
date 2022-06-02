587447298b6b2344d633e3b63c7be153c9da077e
bbpardo <b.b.pardo@protonmail.com>
2022-06-02 09:46:14 +0100

    fasjdfha

---
+++ proyecto-final/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <header>
+      <button className='botonMenu'>
+      </button>
+      <nav className='principal'>
+          <ul>
+              <li><a href="#">Lorem.</a></li>
+              <li><a href="#">Soluta!</a></li>
+              <li><a href="#">Reprehenderit.</a></li>
+              <li><a href="#">Nulla!</a></li>
+              <li><a href="#">Ipsum.</a></li>
+          </ul>
+      </nav>
+    </header>   
+    </div>
+  );
+}
+
+export default App;
